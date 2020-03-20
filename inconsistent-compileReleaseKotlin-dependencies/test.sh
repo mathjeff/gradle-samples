539f4c04b55cb1a9289b2e4c861abc8ed351d6a1
Jeff Gaston <jeffrygaston@google.com>
2020-03-20 12:57:41 -0400

    Updating wording of output of inconsistent-compileReleaseKotlin-dependencies/test.sh

--- inconsistent-compileReleaseKotlin-dependencies/test.sh
+++ inconsistent-compileReleaseKotlin-dependencies/test.sh
@@ -10,10 +10,10 @@ mkdir -p $OUT_DIR
 
 echo starting first build
 ./gradlew --no-daemon                :activity:activity-ktx:compileReleaseKotlin :lifecycle:lifecycle-livedata:doResolve 2>&1 | tee log
-if grep 'task task.*does not contain' log; then
-  echo first grep passed, continuing to second build
+if grep 'fail - task task.*does not contain' log; then
+  echo "First grep gave expected result, continuing to second build"
 else
-  echo first grep failed
+  echo "First grep failed. Maybe the problem has been fixed? Yippee!"
   exit 1
 fi
 
@@ -22,9 +22,18 @@ rm out -rf
 echo
 echo starting second build
 ./gradlew --no-daemon -PresolveFirst :activity:activity-ktx:compileReleaseKotlin :lifecycle:lifecycle-livedata:doResolve 2>&1 | tee log2
-if grep 'task task.*contains' log2; then
-  echo test.sh passed
+
+if grep 'pass - task task.*contains' log2; then
+  echo
+  echo "test.sh reproduced the problem."
+  echo "Inputs of :activity:activity-ktx:compileReleaseKotlin seem to be affected by the presence or absence of the -PresolveFirst property"
+  echo
+  echo "Specifically, it seems that:"
+  echo "  Task :activity:activity-ktx:compileReleaseKotlin"
+  echo "  declares core-runtime-2.1.0-api.jar as an input if and only if"
+  echo "  :androidx.lifecycle:lifecycle-livedata-core has already been resolved before the execution of this task"
 else
-  echo second grep failed
+  echo
+  echo "Second grep failed. Maybe the problem has been fixed? Yippee!"
   exit 1
 fi
